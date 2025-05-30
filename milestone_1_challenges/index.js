3fc01ea79ff42875e0d652dae8d4e15d7657b827
Nxsh <nathanael.mutua.m@gmail.com>
2025-05-30 16:22:23 +0300

    complete Challenge3: Perimeter of a REctangle
    - add function declaration 'findPerimeter', that takes 2 parameters
    (length and width).
    - add local scope variable 'perimeter' that holds the result for
    (2*(l+w))
    - add console.log() to output result when functionis declared.

--- milestone_1_challenges/index.js
+++ milestone_1_challenges/index.js
@@ -3,20 +3,26 @@ function addition(num1, num2) {
     var sum = num1 + num2;
     return sum;
 }
-var additionResult = addition(3, 5);
+var additionResult = addition(3, 5); // 3 + 5 = 8
 console.log(additionResult);
 var addition2 = function (number1, number2) {
     return number1 + number2;
 };
-console.log("Addition2: ".concat(addition2(3, 5)));
+console.log("Addition2: ".concat(addition2(3, 5))); // 3 + 5 = 8
 console.log("---------------------------");
 // Challenge:2 Convert Minutes into Seconds
 function convert(min) { return min * 60; }
 ;
 var minuteConversion = convert(5);
-console.log(minuteConversion);
+console.log(minuteConversion); // 5min * 60 = 300seconds
 console.log("---------------------------");
 // Challenge:3 Perimeter of a Rectangle
+var findPerimeter = function (length, width) {
+    var perimeter = (2 * (length + width));
+    console.log(perimeter);
+};
+findPerimeter(6, 7); // 26
+findPerimeter(20, 10); // 60
 console.log("---------------------------");
 // Challenge:4 Check Negative
 console.log("---------------------------");
--- milestone_1_challenges/index.ts
+++ milestone_1_challenges/index.ts
@@ -3,8 +3,8 @@ function addition(num1: number, num2: number){
     let sum = num1 + num2;
     return sum
 }
-const additionResult = addition(3,5);
-console.log(additionResult);
+const additionResult = addition(3,5); // 3 + 5 = 8
+console.log(additionResult); 
 
 // also another answer to Challenge 1 using function type signatures
 
@@ -13,17 +13,24 @@ type mathOperation = (num1: number, num2: number) => number;
 const addition2: mathOperation = function(number1, number2){
     return number1 + number2;
 }
-console.log(`Addition2: ${addition2(3,5)}`)
+console.log(`Addition2: ${addition2(3,5)}`) // 3 + 5 = 8
 
 console.log("---------------------------")
 // Challenge:2 Convert Minutes into Seconds
 
 function convert(min: number){ return min * 60};
 const minuteConversion  = convert(5);
-console.log(minuteConversion);  
+console.log(minuteConversion); // 5min * 60 = 300seconds
 
 console.log("---------------------------")
 // Challenge:3 Perimeter of a Rectangle
+
+let findPerimeter = function(length: number, width: number){
+    let perimeter = (2 * (length + width));
+    console.log(perimeter);
+}
+findPerimeter(6, 7); // 26
+findPerimeter(20,10); // 60
 
 console.log("---------------------------")
 // Challenge:4 Check Negative
