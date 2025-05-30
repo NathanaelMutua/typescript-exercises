a3315c17af4b27f6b689337b9a93c26290d2048d
Nxsh <nathanael.mutua.m@gmail.com>
2025-05-30 15:16:19 +0300

    complete Challenge1: Sum of Two Numbers
    - add 'addition' function
    - add const additionResult to store the result of the function carried
    out on 3 and 5

---
+++ milestone_1_challenges/index.js
@@ -0,0 +1,6 @@
+function addition(num1, num2) {
+    var sum = num1 + num2;
+    return sum;
+}
+var additionResult = addition(3, 5);
+console.log(additionResult);
---
+++ milestone_1_challenges/index.ts
@@ -0,0 +1,22 @@
+// Challenge 1: Return The Sum of Two Numbers
+function addition(num1: number, num2: number){
+    let sum = num1 + num2;
+    return sum
+}
+const additionResult = addition(3,5);
+console.log(additionResult);
+
+// Challenge:2 Convert Minutes into Seconds
+// Challenge:3 Perimeter of a Rectangle
+// Challenge:4 Check Negative
+// Challenge:5 Can I Drive
+// Challenge:6 Largest Number
+// Challenge:7 BMI Calculator
+// Challenge:8 Greeting Based On Time
+// Challenge:9 FizzBuzz
+// Challenge:10 Perimeter 2
+// Challenge:11 Sum of Even Numbers
+// Challenge:12 Multiply by Itself
+// Challenge:13 Factorial Calculator
+// Challenge:14 Multiple Sum
+// Challenge:15 Sum of Digits
