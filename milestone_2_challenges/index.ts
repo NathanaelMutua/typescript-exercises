9425bc4572c4e77cd3ae04fd7ef515cc29929235
Nxsh <nathanael.mutua.m@gmail.com>
2025-06-01 21:19:06 +0300

    complete Challenge1: Sum Of Positives
    - add function type signature 'sumFunction' to pre-specify the data types
    - add local variable 'sum' to track sum of all positive numbers.
    - add for loop to loop through array
    - add nested if to check positive value condition
    - return sum

---
+++ milestone_2_challenges/index.ts
@@ -0,0 +1,60 @@
+// Challenge1: Sum of Positives
+
+type sumFunction = (array: number[]) => number;
+
+const sumOfPositives: sumFunction = function(array){
+    let sum: number = 0;
+    for (let i = 0; i < array.length; i++){
+        if (array[i] > 0){
+            sum += array[i];
+        }
+    }
+    return sum
+}
+console.log(sumOfPositives([1, -3, 5, -2, 9, -8])); // 15
+console.log(sumOfPositives([2, -2, -4, -20, 10, -8])); //12
+
+console.log("---------------------")
+// Challenge2: Find Maximum Value
+
+console.log("---------------------")
+// Challenge3: Election winner
+
+console.log("---------------------")
+// Challenge4: Longest word
+
+console.log("---------------------")
+// Challenge5: Count Properties
+
+console.log("---------------------")
+// Challenge6: Filter by Length
+
+console.log("---------------------")
+// Challenge7: Sum of Even Numbers
+
+console.log("---------------------")
+// Challenge8: Difference Between Sum of Even and Odd Numbers
+
+console.log("---------------------")
+// Challenge9: Count Truthy
+
+console.log("---------------------")
+// Challenge10: Average of Numbers
+
+console.log("---------------------")
+// Challenge11: Linear Search
+
+console.log("---------------------")
+// Challenge12: Reverse Linear Search
+
+console.log("---------------------")
+// Challenge13: Linear Search All Indices
+
+console.log("---------------------")
+// Challenge14: Count Occurrences
+
+console.log("---------------------")
+// Challenge15: Remove Duplicates
+
+console.log("---------------------")
+// Challenge16: Most Frequent
