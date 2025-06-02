f355d9689812b14b7725d7cc3e0aac96279054df
Nxsh <nathanael.mutua.m@gmail.com>
2025-06-02 18:15:30 +0300

    complete Challenge4: Longest word
    - add function declaration 'findLongestWord'
    - add local variables:
        - newReversedArray: holds the reversed array accepted as an argument
        - largestWord: holds the largest word value as we loop through
        - position: holds the position in the array of the largest word
    - add if condition within the for loop to check if the current value is
    the largest.
    - output to console the largest word.

--- milestone_2_challenges/index.ts
+++ milestone_2_challenges/index.ts
@@ -2,51 +2,51 @@
 
 type sumFunction = (array: number[]) => number;
 
-const sumOfPositives: sumFunction = function(array){
-    let sum: number = 0;
-    for (let i = 0; i < array.length; i++){
-        if (array[i] > 0){
-            sum += array[i];
-        }
+const sumOfPositives: sumFunction = function (array) {
+  let sum: number = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > 0) {
+      sum += array[i];
     }
-    return sum
-}
+  }
+  return sum;
+};
 console.log(sumOfPositives([1, -3, 5, -2, 9, -8])); // 15
 console.log(sumOfPositives([2, -2, -4, -20, 10, -8])); //12
 
-console.log("---------------------")
+console.log("---------------------");
 // Challenge2: Find Maximum Value
 
-function findMax(array: number[]){
-    let maxNumber: number = array[0];
-    for (let i = 1; i < array.length; i++){
-        if (maxNumber < array[i]){
-            maxNumber = array[i];
-        } else {
-            continue
-        }
+function findMax(array: number[]) {
+  let maxNumber: number = array[0];
+  for (let i = 1; i < array.length; i++) {
+    if (maxNumber < array[i]) {
+      maxNumber = array[i];
+    } else {
+      continue;
     }
-    console.log(maxNumber);
+  }
+  console.log(maxNumber);
 }
-findMax([3, 7, 2, 9, 5]);// returns: 9
+findMax([3, 7, 2, 9, 5]); // returns: 9
 findMax([43, 3, 210, 90, 12, 20]); // returns: 210
 
-console.log("---------------------")
+console.log("---------------------");
 // Challenge3: Election winner
 
-type Candidate = {name: string, votes: number};
+type Candidate = { name: string; votes: number };
 
-let findWinner = function(array: Candidate[]){
-    let winner = array[0];
-    let maxVotes: number = 0;
-    for (let i = 0; i < array.length; i++){
-        if (array[i].votes > maxVotes){
-            maxVotes = array[i].votes;
-            winner = array[i];
-        }
+let findWinner = function (array: Candidate[]) {
+  let winner = array[0];
+  let maxVotes: number = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].votes > maxVotes) {
+      maxVotes = array[i].votes;
+      winner = array[i];
     }
-    console.log(winner)
-}
+  }
+  console.log(winner);
+};
 const candidates = [
   { name: "Alice", votes: 50 },
   { name: "Bob", votes: 75 },
@@ -54,41 +54,56 @@ const candidates = [
 ];
 findWinner(candidates); // { name: "Bob", votes: 75 }
 
-console.log("---------------------")
+console.log("---------------------");
 // Challenge4: Longest word
 
-console.log("---------------------")
+function findLongestWord(array: string []){
+    let newReversedArray = array.reverse();
+    let largestWord: string = newReversedArray[0];// this will ensure the last arrived at largestWord value will be the first occurring one. 
+    let position: number = 0;
+
+    for (let i = 1; i < newReversedArray.length; i++){ // alternatively, I think we could have started looping from the last value to the front [0] position
+        if (newReversedArray[i].length > newReversedArray.length){
+            largestWord = newReversedArray[i];
+            position = i;
+        }
+    }
+    console.log(`${largestWord} position[${position}]`);
+}
+findLongestWord(["apple", "banana", "pear", "grapefruit", "peaches", "guava", "grapefruit"]); // grapefruit position[3]
+
+console.log("---------------------");
 // Challenge5: Count Properties
 
-console.log("---------------------")
+console.log("---------------------");
 // Challenge6: Filter by Length
 
-console.log("---------------------")
+console.log("---------------------");
 // Challenge7: Sum of Even Numbers
 
-console.log("---------------------")
+console.log("---------------------");
 // Challenge8: Difference Between Sum of Even and Odd Numbers
 
-console.log("---------------------")
+console.log("---------------------");
 // Challenge9: Count Truthy
 
-console.log("---------------------")
+console.log("---------------------");
 // Challenge10: Average of Numbers
 
-console.log("---------------------")
+console.log("---------------------");
 // Challenge11: Linear Search
 
-console.log("---------------------")
+console.log("---------------------");
 // Challenge12: Reverse Linear Search
 
-console.log("---------------------")
+console.log("---------------------");
 // Challenge13: Linear Search All Indices
 
-console.log("---------------------")
+console.log("---------------------");
 // Challenge14: Count Occurrences
 
-console.log("---------------------")
+console.log("---------------------");
 // Challenge15: Remove Duplicates
 
-console.log("---------------------")
+console.log("---------------------");
 // Challenge16: Most Frequent
